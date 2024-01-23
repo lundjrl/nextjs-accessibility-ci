99c9149a07c8cc7c27cf54490ae61f8c247a5b1e
lundjrl <jrlbidamin@gmail.com>
2024-01-23 15:13:41 -0500

    Fix for test in terminal logging cy file

--- cypress/e2e/log-to-terminal.cy.ts
+++ cypress/e2e/log-to-terminal.cy.ts
@@ -19,7 +19,7 @@ export const terminalLog = violations => {
 
 // Then in your test...
 it('Logs violations to the terminal', () => {
-  cy.visit('http://localhost:3000/fail')
+  cy.visit('http://localhost:3000/')
   cy.injectAxe()
   cy.checkA11y(undefined, undefined, terminalLog)
 })
